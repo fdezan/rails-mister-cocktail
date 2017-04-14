559b063cd61321e1713f8f08aa1ce28202f0e3c3
fdezan <fabio.dezan@gmail.com>
2017-04-14 15:26:56 -0300

    correction <div>

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,4 +2,10 @@
 
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+  class MyUploader < CarrierWave::Uploader::Base
+  def default_url(*args)
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  end
+end
+
 end
